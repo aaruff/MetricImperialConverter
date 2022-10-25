edfae9bd1859b0de6681355de4e7732a5c2ef030
aaruff <anwarruff@gmail.com>
2022-10-25 17:55:49 -0400

    Initial Commit

---
+++ MetricImperialConverter/MetricImperialConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  MetricImperialConverterApp.swift
+//  MetricImperialConverter
+//
+//  Created by Anwar Ruff on 10/25/22.
+//
+
+import SwiftUI
+
+@main
+struct MetricImperialConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
